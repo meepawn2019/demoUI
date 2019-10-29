035fbf3353bee0ccdf82e1620ab3fc1f0f79e6aa
meepawn2019 <meepawn2018@gmail.com>
2019-10-30 00:04:09 +0700

    xong history

---
+++ public/server/models/getChatroomHistory.js
@@ -0,0 +1,12 @@
+import conn from './connection';
+
+/* Truy vấn vào database lấy tin nhắn theo chatroom. !!!Không thể dùng return tại hàm này */
+module.exports = (callback) => {
+    const sql = 'select * from message where to_chatroom = 1';
+    conn.aquire((err, con) => {
+        con.query(sql, (err, result, fields) => {
+            //console.log(result);
+            callback(null, result);
+        });
+    });
+};
--- public/server/models/msgDB.js
+++ public/server/models/msgDB.js
@@ -1,11 +1,12 @@
 import conn from './connection';
 
+/* Thêm msg vào trong database. */
 module.exports = (msg) =>{
     let username = msg.username;
     let message = msg.text;
     const sql = 'INSERT INTO message (content, from_user, to_chatroom) VALUES ?;';
     let value = [
-        [message, msg.id, 1]
+        [message, username, 1]
     ];
     conn.aquire((err, con) => {
         con.query(sql, [value], (err, result, fields) => {
